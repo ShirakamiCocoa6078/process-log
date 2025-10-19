f4ca47f5945990cdeeabc09d792a6963a311e573
Shirakami_Cocoa6078 <71052778+ShirakamiCocoa6078@users.noreply.github.com>
2025-10-19 21:59:39 +0900

    비어있는 interface User 블록을 삭제

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -12,14 +12,9 @@ declare module 'next-auth/jwt' {
 
 // Session의 user 객체에 id와 role을 포함하도록 확장
 declare module 'next-auth' {
-  interface Session {
-    user: {
-      id: string; // 👈 여기! user 객체에 id를 추가합니다.
-    } & DefaultSession['user']; // 기존의 name, email, image 타입은 유지
-  }
-
-  // (선택) User 모델에도 role을 추가하고 싶다면
-  interface User extends DefaultUser {
-    // role: string;
-  }
+    interface Session {
+        user: {
+            id: string;
+        } & DefaultSession['user'];
+    }
 }
